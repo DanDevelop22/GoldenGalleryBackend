e7480a2564da15abbfab98898b1a9bc86d22418a
darielaguilar <darielaguilar2012@gmail.com>
2022-07-27 17:03:53 -0400

    Añadido al modelo Paint el campo bool on_sell

--- authentication/models.py
+++ authentication/models.py
@@ -90,6 +90,7 @@ class Paint(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2,null=True)
     description = models.TextField(blank=True,null=True)
     #user = models.OneToOneField(UserProfile,on_delete=models.CASCADE,related_name='paint', unique=False, blank=True, null=True)
+    on_sell = models.BooleanField(default=False)
     user = models.ForeignKey(UserProfile,on_delete=models.CASCADE,related_name='paint', unique=False, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
